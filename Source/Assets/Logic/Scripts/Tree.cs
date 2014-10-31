6c29b87087e9eeb2295aa2feaf7aa70b2b4aa1a0
Lavrov Sergey <lavrov-sergey@yandex.ru>
2014-10-31 23:49:15 +0300

    2014-10-09 Очистка модулей. Добавлены модели сундука, ловушки и деревьев.

--- Source/Assets/Logic/Scripts/Tree.cs
+++ Source/Assets/Logic/Scripts/Tree.cs
@@ -1,27 +1,49 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 
 public class Tree : MonoBehaviour {
 
 	private int Random_Value =0;
-	public SpriteRenderer Tree_Sprite ;
-	public Sprite sprite1; 
-	public Sprite sprite2;
+
+	
+	public MeshFilter   Tree_Mesh_Filter;
+	public Animator     Tree_Animator;
+	public Transform    Tree_Scale;
+
+	public Mesh Tree_Model_1;
+	public Mesh Tree_Model_2;
+
+	public Avatar Tree_Animation_1;
+	public Avatar Tree_Animation_2;
 
 	// Use this for initialization
 	void Start () {
 
-		Tree_Sprite = GetComponent<SpriteRenderer>(); 
+		Tree_Mesh_Filter = GetComponent<MeshFilter>();
+		Tree_Animator = GetComponent<Animator>();
+		 
 		Random_Value = Random.Range (1,3);
 
+	
 		if (Random_Value == 1)
 		{
-			Tree_Sprite.sprite = sprite1;
+			//Tree_Sprite.sprite = sprite1;
+			Tree_Mesh_Filter.mesh = Tree_Model_1;
+			Tree_Animator.avatar = Tree_Animation_1;
+			Tree_Scale.localScale = new Vector3 (3,3,3);  
+
+
+
+
 		}
 
 		if (Random_Value == 2)
 		{
-			Tree_Sprite.sprite = sprite2;
+			//Tree_Sprite.sprite = sprite2;
+			Tree_Mesh_Filter.mesh = Tree_Model_2;
+			Tree_Animator.avatar = Tree_Animation_2;
+			Tree_Scale.localScale = new Vector3 (3,3,3);  
 		}
 
 	
