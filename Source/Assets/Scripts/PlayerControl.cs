ee061b84cbe3fa635bb2f67326f2d7c76c2c9a6c
Lavrov Sergey <lavrov-sergey@yandex.ru>
2014-10-31 22:57:50 +0300

    2014-10-02 Прототип 2D, созданный на основе бумажного. 4 уровня. Плоская кошка двигается стрелками. Препятствия: болото, деревья, сундук.

---
+++ Source/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float maxSpeed = 3f;
+
+	// При подготовке к обновлению
+	void FixedUpdate () {
+		// Нажатие клавиш управлени курсором на клавиатуре приводит к движению персонажа
+		float moveX = Input.GetAxis ("Horizontal");
+		float moveY = Input.GetAxis ("Vertical");
+		rigidbody2D.velocity = new Vector2 (moveX * maxSpeed, moveY * maxSpeed);
+	}
+}
