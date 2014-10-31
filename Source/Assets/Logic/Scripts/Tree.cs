ac831604d81d930eae0bc92ed1a6f28e1b4bc678
Lavrov Sergey <lavrov-sergey@yandex.ru>
2014-10-31 23:36:56 +0300

    2014-10-07 Появилось дерево. Воин стал корректнее бегать за кошкой. Добавилось рандомная генерация вида дерева.

---
+++ Source/Assets/Logic/Scripts/Tree.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tree : MonoBehaviour {
+
+	private int Random_Value =0;
+	public SpriteRenderer Tree_Sprite ;
+	public Sprite sprite1; 
+	public Sprite sprite2;
+
+	// Use this for initialization
+	void Start () {
+
+		Tree_Sprite = GetComponent<SpriteRenderer>(); 
+		Random_Value = Random.Range (1,3);
+
+		if (Random_Value == 1)
+		{
+			Tree_Sprite.sprite = sprite1;
+		}
+
+		if (Random_Value == 2)
+		{
+			Tree_Sprite.sprite = sprite2;
+		}
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	/*void OnTriggerEnter (Collider tr)
+	{
+		if (tr.collider.tag == "Player") 
+		{
+
+		}
+	}*/
+}
